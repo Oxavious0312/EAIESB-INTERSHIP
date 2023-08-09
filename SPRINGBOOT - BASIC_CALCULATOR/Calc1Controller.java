2242fab692d090d59c7a321c04c682fd7f2abd72
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2023-08-09 16:07:12 +0530

    Update Calc1Controller.java

--- SPRINGBOOT - BASIC_CALCULATOR/Calc1Controller.java
+++ SPRINGBOOT - BASIC_CALCULATOR/Calc1Controller.java
@@ -1,15 +1,21 @@
-package com.calculator1.calc1;
+package com.eaiesb;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Calc1Controller {
-    @GetMapping("/add")            // add is to be included in the url
-    public int addcalc(@RequestParam int num1 , @RequestParam int num2){
-        return (num1+num2);
-    }
+public class CalculatorController {
+	
+	@GetMapping("/add")
+		public int addcalc(@RequestParam int num1 , @RequestParam int num2){
+	        return (num1+num2);
+	    }
+	
     @GetMapping("/sub")            // add is to be included in the url
     public int subcalc(@RequestParam int num1 , @RequestParam int num2){
         return (num1-num2);
@@ -21,5 +27,47 @@ public class Calc1Controller {
     @GetMapping("/div")            // add is to be included in the url
     public int divcalc(@RequestParam int num1 , @RequestParam int num2){
         return (num1/num2);
+    }
+    
+    @GetMapping("/add/{num1}/{num2}")
+	public int addUri(@PathVariable int num1 , @PathVariable int num2){
+        return (num1+num2);
+    }
+    
+    @GetMapping("/sub/{num1}/{num2}")
+	public int subUri(@PathVariable int num1 , @PathVariable int num2){
+        return (num1-num2);
+    }
+
+    
+    @GetMapping("/mul/{num1}/{num2}")
+	public int mulUri(@PathVariable int num1 , @PathVariable int num2){
+        return (num1*num2);
+    }
+
+    
+    @GetMapping("/div/{num1}/{num2}")
+	public int divUri(@PathVariable int num1 , @PathVariable int num2){
+        return (num1/num2);
+    }
+    
+    @PostMapping("/add")
+    public int add(@Validated @RequestBody Calculator cal) {
+    	return (cal.getNum1()+ cal.getNum2());
+    }
+    
+    @PostMapping("/sub")
+    public int sub(@Validated @RequestBody Calculator cal) {
+    	return (cal.getNum1()+ cal.getNum2());
+    }
+    
+    @PostMapping("/mul")
+    public int mul(@Validated @RequestBody Calculator cal) {
+    	return (cal.getNum1()+ cal.getNum2());
+    }
+    
+    @PostMapping("/div")
+    public int div(@Validated @RequestBody Calculator cal) {
+    	return (cal.getNum1()+ cal.getNum2());
     }
 }
