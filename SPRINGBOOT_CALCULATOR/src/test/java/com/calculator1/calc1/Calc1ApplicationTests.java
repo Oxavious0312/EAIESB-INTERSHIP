f369a1d8341d3b1e230fa9568f93616dc9d8cb6f
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2023-08-11 15:23:22 +0530

    Add files via upload

---
+++ SPRINGBOOT_CALCULATOR/src/test/java/com/calculator1/calc1/Calc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator1.calc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
