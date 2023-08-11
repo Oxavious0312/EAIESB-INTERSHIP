0f9140644801373d45944f52c9a65bd510ac5754
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2023-08-11 15:25:01 +0530

    Add files via upload

---
+++ SPRINGBOOT_SIMPLEINTREST/src/test/java/com/simpleintest/sip1/Sip1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleintest.sip1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sip1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
